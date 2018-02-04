f17e083d8ae75c7613b147b673a5b7acd2731519
BaltazarUA <oleg734437@gmail.com>
2018-02-04 22:42:22 +0200

    Added new app and a some databases

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def about(request):
-    return HttpResponse("<h1>Info about my blog</h1>")
+    return render(request, "about.html")
+    #return HttpResponse("<h1>Info about my blog</h1>")
 
 def homepage(request):
-    return HttpResponse("home")
+    return render(request, "homepage.html")
+    #return HttpResponse("home")
